102a09f0e281d8e280b03338f62427e957038e37
AlSovPich <58888340+AlSovPich@users.noreply.github.com>
2020-01-12 14:45:08 +0300

    Add files via upload

---
+++ christmastree2.py
@@ -0,0 +1,11 @@
+word1 = input()
+word2 = input()
+word3 = input()
+if ('раз' == word1 or '1' == word1):
+    if ('раз' == word1 and 'два' == word2 and 'три' == word3):
+       print('ГОРИ')
+    else:
+        if ('1' == word1 and '2' == word2 and '3' == word3):
+            print('ГОРИ')
+        else:
+            print('НЕ ГОРИ')
